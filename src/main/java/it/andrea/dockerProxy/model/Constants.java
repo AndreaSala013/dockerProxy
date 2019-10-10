609dfe1d1ec228790a0bb90636aa11432301ae69
Andrea Sala <andrea.sala013@gmail.com>
2019-10-10 17:57:02 +0200

    cambio indirizzo per docker

--- src/main/java/it/andrea/dockerProxy/model/Constants.java
+++ src/main/java/it/andrea/dockerProxy/model/Constants.java
@@ -1,8 +1,8 @@
 package it.andrea.dockerProxy.model;
 
 public class Constants {
-    //public static final String PORTAINER_BASE_URL = "http://portainer:9000/";
-    public static final String PORTAINER_BASE_URL = "http://localhost:9000/";
+    public static final String PORTAINER_BASE_URL = "http://portainer:9000/";
+    //public static final String PORTAINER_BASE_URL = "http://localhost:9000/";
     public static final String PORTAINER_AUTH_URL = PORTAINER_BASE_URL + "api/auth";
     public static final String PORTAINER_URL_CONTAINER_LIST = PORTAINER_BASE_URL + "api/endpoints/1/docker/containers/json";
     public static final String PORTAINER_USERNAME = "admin";
