c5bffa22298ec7929516862ff43be67129ed19d5
Andrea Sala <andrea.sala013@gmail.com>
2019-10-25 11:38:02 +0200

    Fine

--- src/main/java/it/andrea/dockerProxy/DockerProxyController.java
+++ src/main/java/it/andrea/dockerProxy/DockerProxyController.java
@@ -5,6 +5,7 @@ import it.andrea.dockerProxy.model.ProxyResponse;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
@@ -12,6 +13,11 @@ import org.springframework.web.bind.annotation.*;
 import org.apache.http.impl.client.CloseableHttpClient;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 @RestController
 public class DockerProxyController {
@@ -25,19 +31,72 @@ public class DockerProxyController {
         StringEntity params = new StringEntity(json.toString());
         httppost.setEntity(params);
         HttpResponse response = httpclient.execute(httppost);
-        ProxyResponse servletResponse = new ProxyResponse(response.getStatusLine().getStatusCode(), EntityUtils.toString(response.getEntity()));
-        return servletResponse;
+        ProxyResponse proxyResponse = new ProxyResponse(response.getStatusLine().getStatusCode(), EntityUtils.toString(response.getEntity()));
+        return proxyResponse;
     }
 
     @CrossOrigin
-    @GetMapping("/listContainers")
-    public ProxyResponse listContainers(@RequestParam String jwtToken) throws IOException {
+    @PostMapping("/listContainers")
+    public ProxyResponse listContainers(@RequestParam String jwtToken, @RequestParam String filter) throws IOException, URISyntaxException {
         CloseableHttpClient httpclient = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet(Constants.PORTAINER_URL_CONTAINER_LIST);
-        httpGet.setHeader("Authorization", "Bearer " + jwtToken);
-        HttpResponse response = httpclient.execute(httpGet);
-        ProxyResponse servletResponse = new ProxyResponse(response.getStatusLine().getStatusCode(), EntityUtils.toString(response.getEntity()));
-        return servletResponse;
+        URIBuilder uriBuilder = new URIBuilder(httpGet.getURI());
+        uriBuilder.addParameter("all","true");
+        if(filter == null || filter.equals(Constants.NULL_STR) || filter.isEmpty()){
+            ProxyResponse proxyResponse = new ProxyResponse(400, "{'err':'Filtro lista conatiner non impostato'}");
+            return proxyResponse;
+        }else{
+            if(!filter.equals(Constants.FILTER_CONTAINER_ALL)){
+                List<String> listFilter = Arrays.asList(filter.split(Constants.CONTAINER_FILTER_SEPARATOR));
+                StringBuilder value = new StringBuilder();
+                Iterator<String> iterator = listFilter.iterator();
+                while (iterator.hasNext()){
+                    value.append("\""+iterator.next()+"\"");
+                    if(iterator.hasNext()){
+                        value.append(Constants.CONTAINER_FILTER_SEPARATOR);
+                    }
+                }
+                uriBuilder.addParameter("filters","{\"name\":["+value+"]}");
+            }
+            URI uri = uriBuilder.build();
+            httpGet.setURI(uri);
+            httpGet.setHeader("Authorization", "Bearer " + jwtToken);
+            HttpResponse response = httpclient.execute(httpGet);
+            ProxyResponse proxyResponse = new ProxyResponse(response.getStatusLine().getStatusCode(), EntityUtils.toString(response.getEntity()));
+            return proxyResponse;
+        }
     }
 
+    @CrossOrigin
+    @PostMapping("/startContainer")
+    public ProxyResponse startContainer(@RequestParam String jwtToken, @RequestParam String idContainer) throws IOException, URISyntaxException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpPost httppost = new HttpPost(Constants.PORTAINER_URL_START_CONTAINER+idContainer+"/start");
+        httppost.setHeader("Authorization", "Bearer " + jwtToken);
+        HttpResponse response = httpclient.execute(httppost);
+        ProxyResponse proxyResponse;
+        if(response.getStatusLine().getStatusCode() == 204){
+
+            proxyResponse = new ProxyResponse(response.getStatusLine().getStatusCode(),"");
+        }else{
+            proxyResponse = new ProxyResponse(response.getStatusLine().getStatusCode(), EntityUtils.toString(response.getEntity()));
+        }
+        return proxyResponse;
+    }
+
+    @CrossOrigin
+    @PostMapping("/stopContainer")
+    public ProxyResponse stopContainer(@RequestParam String jwtToken, @RequestParam String idContainer) throws IOException, URISyntaxException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpPost httppost = new HttpPost(Constants.PORTAINER_URL_START_CONTAINER+idContainer+"/stop");
+        httppost.setHeader("Authorization", "Bearer " + jwtToken);
+        HttpResponse response = httpclient.execute(httppost);
+        ProxyResponse proxyResponse;
+        if(response.getStatusLine().getStatusCode() == 204){
+            proxyResponse = new ProxyResponse(response.getStatusLine().getStatusCode(),"");
+        }else{
+            proxyResponse = new ProxyResponse(response.getStatusLine().getStatusCode(), EntityUtils.toString(response.getEntity()));
+        }
+        return proxyResponse;
+    }
 }
--- src/main/java/it/andrea/dockerProxy/DockerProxyMain.java
+++ src/main/java/it/andrea/dockerProxy/DockerProxyMain.java
@@ -2,11 +2,18 @@ package it.andrea.dockerProxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class DockerProxyMain extends SpringBootServletInitializer {
+
     public static void main(String[] args) {
         SpringApplication.run(DockerProxyMain.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DockerProxyMain.class);
     }
 }
