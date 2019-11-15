8b3b6dfbedf026764f9ec2bfe1a33c6d4036230f
Andrea Sala <andrea.sala013@gmail.com>
2019-11-15 09:20:15 +0100

    modifiche alle costanti

--- src/main/java/it/andrea/dockerProxy/model/Constants.java
+++ src/main/java/it/andrea/dockerProxy/model/Constants.java
@@ -2,20 +2,21 @@ package it.andrea.dockerProxy.model;
 
 public class Constants {
     //url dastan
-    //public static final String PORTAINER_BASE_URL = "http://app.dastan.eng.it:9000/";
+    public static final String PORTAINER_BASE_URL = "http://app.dastan.eng.it:9000/";
     //url locale docker
-    public static final String PORTAINER_BASE_URL = "http://portainer:9000/";
+    //public static final String PORTAINER_BASE_URL = "http://portainer:9000/";
     //url locale
     //public static final String PORTAINER_BASE_URL = "http://localhost:9000/";
     public static final String PORTAINER_AUTH_URL = PORTAINER_BASE_URL + "api/auth";
     public static final String PORTAINER_URL_CONTAINER_LIST = PORTAINER_BASE_URL + "api/endpoints/1/docker/containers/json";
     public static final String PORTAINER_URL_START_CONTAINER = PORTAINER_BASE_URL + "api/endpoints/1/docker/containers/";
     //credenziali in locale
-    public static final String PORTAINER_USERNAME = "admin";
-    public static final String PORTAINER_PASSWORD = "rootPassword";
-    //credenziali dastan
     //public static final String PORTAINER_USERNAME = "admin";
-    //public static final String PORTAINER_PASSWORD = "Settembre2019";
+    //public static final String PORTAINER_PASSWORD = "rootPassword";
+    //credenziali dastan
+    public static final String PORTAINER_USERNAME = "admin";
+    public static final String PORTAINER_PASSWORD = "Settembre2019";
+
     public static final String CONTAINER_FILTER_SEPARATOR = ",";
     public static final String FILTER_CONTAINER_ALL = "ALL";
     public static final String NULL_STR = "null";
